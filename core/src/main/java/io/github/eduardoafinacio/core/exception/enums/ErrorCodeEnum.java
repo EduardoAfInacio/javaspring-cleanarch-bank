701f2799d98189cfed88b948599ef29b10a89279
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-08 18:23:12 -0300

    Updating ErrorCodeEnum attributes and methods.

--- core/src/main/java/io/github/eduardoafinacio/core/exception/enums/ErrorCodeEnum.java
+++ core/src/main/java/io/github/eduardoafinacio/core/exception/enums/ErrorCodeEnum.java
@@ -1,4 +1,29 @@
 package io.github.eduardoafinacio.core.exception.enums;
 
 public enum ErrorCodeEnum {
+    ;
+
+    private String message;
+    private String code;
+
+    ErrorCodeEnum(String message, String code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
