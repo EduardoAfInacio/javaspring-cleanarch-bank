abccc9521c0848a433872cecca9651d2fb7fb8ca
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-08 16:45:35 -0300

    Creating Wallet entity class.

---
+++ core/src/main/java/io/github/eduardoafinacio/core/domain/Wallet.java
@@ -0,0 +1,29 @@
+package io.github.eduardoafinacio.core.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Wallet {
+    private Long id;
+    private BigDecimal balance;
+    private User user;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Wallet(Long id, BigDecimal balance, User user, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.balance = balance;
+        this.user = user;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Wallet(BigDecimal balance, User user) {
+        this.balance = balance;
+        this.user = user;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Wallet() {
+    }
+}
