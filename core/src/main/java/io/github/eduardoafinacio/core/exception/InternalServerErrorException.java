4960d725d8a8ec6b3bb9f818902a881bfc592990
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-08 22:20:49 -0300

    Creating InternalServerErrorException.

---
+++ core/src/main/java/io/github/eduardoafinacio/core/exception/InternalServerErrorException.java
@@ -0,0 +1,10 @@
+package io.github.eduardoafinacio.core.exception;
+
+public class InternalServerErrorException extends Exception {
+    private String code;
+
+    public InternalServerErrorException(String message, String code) {
+        super(message);
+        this.code = code;
+    }
+}
