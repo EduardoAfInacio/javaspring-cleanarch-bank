bd9bebced3b4eb8ede0ac7f7b531ef08b067823d
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-12 14:38:35 -0300

    Implementing FindWalletByTaxNumber UseCase.

---
+++ application/src/main/java/io/github/eduardoafinacio/usecaseimpl/FindWalletByTaxNumberUseCaseImpl.java
@@ -0,0 +1,23 @@
+package io.github.eduardoafinacio.usecaseimpl;
+
+import io.github.eduardoafinacio.core.domain.Wallet;
+import io.github.eduardoafinacio.core.exception.NotFoundException;
+import io.github.eduardoafinacio.core.exception.enums.ErrorCodeEnum;
+import io.github.eduardoafinacio.gateway.FindWalletByTaxNumberGateway;
+import io.github.eduardoafinacio.usecase.FindWalletByTaxNumberUseCase;
+
+public class FindWalletByTaxNumberUseCaseImpl implements FindWalletByTaxNumberUseCase {
+    private FindWalletByTaxNumberGateway findWalletByTaxNumberGateway;
+
+    public FindWalletByTaxNumberUseCaseImpl(FindWalletByTaxNumberGateway findWalletByTaxNumberGateway) {
+        this.findWalletByTaxNumberGateway = findWalletByTaxNumberGateway;
+    }
+    @Override
+    public Wallet findByTaxNumber(String taxNumber) throws NotFoundException{
+        var wallet = findWalletByTaxNumberGateway.findByTaxNumber(taxNumber);
+        if(wallet == null) {
+            throw new NotFoundException(ErrorCodeEnum.WA0001.getMessage(), ErrorCodeEnum.WA0001.getCode());
+        }
+        return wallet;
+    }
+}
--- usecase/src/main/java/io/github/eduardoafinacio/usecase/FindWalletByTaxNumberUseCase.java
+++ usecase/src/main/java/io/github/eduardoafinacio/usecase/FindWalletByTaxNumberUseCase.java
@@ -1,7 +1,8 @@
 package io.github.eduardoafinacio.usecase;
 
 import io.github.eduardoafinacio.core.domain.Wallet;
+import io.github.eduardoafinacio.core.exception.NotFoundException;
 
 public interface FindWalletByTaxNumberUseCase {
-    Wallet findByTaxNumber(String taxNumber);
+    Wallet findByTaxNumber(String taxNumber) throws NotFoundException;
 }
