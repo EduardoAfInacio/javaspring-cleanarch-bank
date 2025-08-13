0f3aa69ca303d99433eaf06bf5fe1ad6764fa59b
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-13 17:59:49 -0300

    Finishing TransactionPinValidateUseCaseImpl.

--- application/src/main/java/io/github/eduardoafinacio/usecaseimpl/TransactionPinValidateUseCaseImpl.java
+++ application/src/main/java/io/github/eduardoafinacio/usecaseimpl/TransactionPinValidateUseCaseImpl.java
@@ -5,17 +5,34 @@ import io.github.eduardoafinacio.core.exception.TransactionPinException;
 import io.github.eduardoafinacio.core.exception.enums.ErrorCodeEnum;
 import io.github.eduardoafinacio.gateway.TransactionPinValidateGateway;
 import io.github.eduardoafinacio.usecase.TransactionPinValidateUseCase;
+import io.github.eduardoafinacio.usecase.UpdateTransactionPinUseCase;
 
 public class TransactionPinValidateUseCaseImpl implements TransactionPinValidateUseCase {
     private TransactionPinValidateGateway transactionPinValidateGateway;
+    private UpdateTransactionPinUseCase updateTransactionPinUseCase;
 
-    public TransactionPinValidateUseCaseImpl(TransactionPinValidateGateway transactionPinValidateGateway) {
+    public TransactionPinValidateUseCaseImpl(TransactionPinValidateGateway transactionPinValidateGateway, UpdateTransactionPinUseCase updateTransactionPinUseCase) {
         this.transactionPinValidateGateway = transactionPinValidateGateway;
+        this.updateTransactionPinUseCase = updateTransactionPinUseCase;
     }
     @Override
-    public void validate(TransactionPin transactionPin) throws TransactionPinException{
-        if(!transactionPinValidateGateway.validate(transactionPin)) {
-            throw new TransactionPinException(ErrorCodeEnum.TRP0003.getMessage(), ErrorCodeEnum.TRP0003.getCode());
+    public Boolean validate(TransactionPin transactionPin) throws TransactionPinException{
+
+        if(transactionPin.getBlocked()){
+            throw new TransactionPinException(ErrorCodeEnum.TRP0002.getMessage(), ErrorCodeEnum.TRP0002.getCode());
         }
+
+        if(!transactionPinValidateGateway.validate(transactionPin)) {
+            transactionPin.setAttempt();
+            var transactionPinUpdated = updateTransactionPinUseCase.update(transactionPin);
+            throw new TransactionPinException(ErrorCodeEnum.TRP0003getMessage(transactionPinUpdated.getAttempt()), ErrorCodeEnum.TRP0003.getCode());
+        }
+
+        if(transactionPin.getAttempt() < 3){
+            transactionPin.resetAttempt();
+            updateTransactionPinUseCase.update(transactionPin);
+        }
+
+        return true;
     }
 }
