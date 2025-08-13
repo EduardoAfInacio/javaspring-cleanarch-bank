481343ad8445b141aaede0f4f524bece7e00ccf7
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-13 13:37:40 -0300

    Adding TransactionPinException to its UseCase Method signature.

--- usecase/src/main/java/io/github/eduardoafinacio/usecase/TransferUseCase.java
+++ usecase/src/main/java/io/github/eduardoafinacio/usecase/TransferUseCase.java
@@ -1,12 +1,9 @@
 package io.github.eduardoafinacio.usecase;
 
-import io.github.eduardoafinacio.core.exception.InternalServerErrorException;
-import io.github.eduardoafinacio.core.exception.NotFoundException;
-import io.github.eduardoafinacio.core.exception.NotificationException;
-import io.github.eduardoafinacio.core.exception.TransferException;
+import io.github.eduardoafinacio.core.exception.*;
 
 import java.math.BigDecimal;
 
 public interface TransferUseCase {
-    Boolean transfer(String fromTaxNumber, String toTaxNumber, BigDecimal value, String pin) throws TransferException, NotFoundException, NotificationException, InternalServerErrorException;
+    Boolean transfer(String fromTaxNumber, String toTaxNumber, BigDecimal value, String pin) throws TransferException, NotFoundException, NotificationException, InternalServerErrorException, TransactionPinException;
 }
