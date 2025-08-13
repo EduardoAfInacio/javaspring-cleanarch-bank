a299d7bb3c666acbd1c4efba160b07ba1be16741
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-13 13:35:37 -0300

    Creating new TransactionPinGateway.

---
+++ application/src/main/java/io/github/eduardoafinacio/gateway/TransactionPinValidateGateway.java
@@ -0,0 +1,7 @@
+package io.github.eduardoafinacio.gateway;
+
+import io.github.eduardoafinacio.core.domain.TransactionPin;
+
+public interface TransactionPinValidateGateway {
+    Boolean validate(TransactionPin transactionPin);
+}
