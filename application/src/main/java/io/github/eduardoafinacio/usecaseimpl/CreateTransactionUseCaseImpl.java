d3b6bbda5b58c9e8110d5b42b7442bd82b4fbb44
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-13 12:41:12 -0300

    Creating CreateTransactionUseCase implementation.

---
+++ application/src/main/java/io/github/eduardoafinacio/usecaseimpl/CreateTransactionUseCaseImpl.java
@@ -0,0 +1,24 @@
+package io.github.eduardoafinacio.usecaseimpl;
+
+import io.github.eduardoafinacio.core.domain.Transaction;
+import io.github.eduardoafinacio.core.exception.TransferException;
+import io.github.eduardoafinacio.core.exception.enums.ErrorCodeEnum;
+import io.github.eduardoafinacio.gateway.CreateTransactionGateway;
+import io.github.eduardoafinacio.usecase.CreateTransactionUseCase;
+
+public class CreateTransactionUseCaseImpl implements CreateTransactionUseCase {
+    private CreateTransactionGateway createTransactionGateway;
+
+    public CreateTransactionUseCaseImpl(CreateTransactionGateway createTransactionGateway) {
+        this.createTransactionGateway = createTransactionGateway;
+    }
+    @Override
+    public Transaction create(Transaction transaction) throws TransferException {
+        var transactionSaved = createTransactionGateway.create(transaction);
+        if(transactionSaved == null) {
+            throw new TransferException(ErrorCodeEnum.TR0003.getMessage(), ErrorCodeEnum.TR0003.getCode());
+        }
+
+        return transactionSaved;
+    }
+}
--- usecase/src/main/java/io/github/eduardoafinacio/usecase/CreateTransactionUseCase.java
+++ usecase/src/main/java/io/github/eduardoafinacio/usecase/CreateTransactionUseCase.java
@@ -1,7 +1,8 @@
 package io.github.eduardoafinacio.usecase;
 
 import io.github.eduardoafinacio.core.domain.Transaction;
+import io.github.eduardoafinacio.core.exception.TransferException;
 
 public interface CreateTransactionUseCase {
-    Transaction create(Transaction transaction);
+    Transaction create(Transaction transaction) throws TransferException;
 }
