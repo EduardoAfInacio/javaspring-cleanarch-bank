5f18e8e3bc50fc2628840d0c9b1856c14fef11fd
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-15 10:56:23 -0300

    Creating TransactionPinEntity entity.

---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/entity/TransactionPinEntity.java
@@ -0,0 +1,33 @@
+package io.github.eduardoafinacio.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TransactionsPin")
+public class TransactionPinEntity {
+    @Column(name = "Id", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Pin", nullable = false)
+    private String pin;
+
+    @Column(name = "Attempt", nullable = false)
+    private String attempt;
+
+    @Column(name = "Blocked", nullable = false)
+    private Boolean blocked;
+
+    @Column(name = "CreatedAt", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "UpdatedAt", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
