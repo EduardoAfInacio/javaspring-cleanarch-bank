80b57a2440437370dedf033da59cc4bbbe1698bc
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-15 11:28:49 -0300

    Creating TransactionEntity entity.

---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/entity/TransactionEntity.java
@@ -0,0 +1,41 @@
+package io.github.eduardoafinacio.entity;
+
+import io.github.eduardoafinacio.core.domain.enums.TransactionStatusEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Transactions")
+public class TransactionEntity {
+    @Column(name = "Id", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "FromWallet")
+    private WalletEntity fromWallet;
+
+    @ManyToOne
+    @JoinColumn(name = "ToWallet")
+    private WalletEntity toWallet;
+
+    @Column(name = "TransactionValue", nullable = false)
+    private BigDecimal transactionValue;
+
+    @Column(name = "Status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionStatusEnum status;
+
+    @Column(name = "CreatedAt", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "UpdatedAt", nullable = false)
+    private LocalDateTime updatedAt;
+}
