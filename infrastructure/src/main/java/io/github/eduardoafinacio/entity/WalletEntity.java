dd4c96e38026a537e88aa7d3e2514ade43b486d8
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-15 11:08:11 -0300

    Creating WalletEntity entity.

---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/entity/WalletEntity.java
@@ -0,0 +1,37 @@
+package io.github.eduardoafinacio.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Wallets")
+public class WalletEntity {
+    @Column(name = "Id", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Balance", nullable = false)
+    private BigDecimal balance;
+
+    @OneToOne
+    @JoinColumn(name = "UserId")
+    private UserEntity userEntity;
+
+    @OneToOne
+    @JoinColumn(name = "TransactionPinId")
+    private TransactionPinEntity transactionPinEntity;
+
+    @Column(name = "CreatedAt", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "UpdatedAt", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
