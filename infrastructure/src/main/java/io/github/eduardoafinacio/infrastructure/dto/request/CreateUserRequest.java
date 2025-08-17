003912047580400bf1f5428cf1f23207ebd487cd
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-17 19:25:40 -0300

    Adding pin to CreateUserRequest parameters.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/dto/request/CreateUserRequest.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/dto/request/CreateUserRequest.java
@@ -2,5 +2,5 @@ package io.github.eduardoafinacio.infrastructure.dto.request;
 
 import io.github.eduardoafinacio.core.domain.enums.UserTypeEnum;
 
-public record CreateUserRequest(String email, String password, String taxNumber, String fullname, UserTypeEnum type) {
+public record CreateUserRequest(String email, String password, String taxNumber, String fullname, UserTypeEnum type, String pin) {
 }
