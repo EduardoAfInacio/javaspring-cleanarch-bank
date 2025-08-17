f022b7c1da863bd9f1037db75a016cd860b7b814
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-17 19:32:41 -0300

    Finishing createUser method in UserController.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/controller/UserController.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/controller/UserController.java
@@ -9,6 +9,7 @@ import io.github.eduardoafinacio.infrastructure.dto.response.BaseResponse;
 import io.github.eduardoafinacio.infrastructure.mapper.UserMapper;
 import io.github.eduardoafinacio.usecase.CreateUserUseCase;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,6 +25,7 @@ public class UserController {
         this.userMapper = userMapper;
     }
 
+    @PostMapping("/createUser")
     public BaseResponse<String> createUser(@RequestBody CreateUserRequest request) throws TaxNumberException, EmailException, InternalServerErrorException, TransactionPinException {
         createUserUseCase.create(userMapper.toUser(request), request.pin());
         return BaseResponse.<String>builder().success(true).message("User created").build();
