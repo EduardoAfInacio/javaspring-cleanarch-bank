490e8ac471aaaa16bc0618afffec9805d4a8812b
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-17 16:46:20 -0300

    Creating CreateUserRequest record.

---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/dto/request/CreateUserRequest.java
@@ -0,0 +1,6 @@
+package io.github.eduardoafinacio.infrastructure.dto.request;
+
+import io.github.eduardoafinacio.core.domain.enums.UserTypeEnum;
+
+public record CreateUserRequest(String email, String password, String taxNumber, String fullname, UserTypeEnum type) {
+}
