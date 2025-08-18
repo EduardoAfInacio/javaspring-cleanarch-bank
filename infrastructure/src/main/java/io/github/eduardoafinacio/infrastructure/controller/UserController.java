b3603708596dcd5e0775b8a3bace857ada3169f9
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-17 21:04:17 -0300

    Changing @Controller annotation to @RestController inside UserController.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/controller/UserController.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/controller/UserController.java
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import static io.github.eduardoafinacio.infrastructure.Utils.Utilities.log;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
