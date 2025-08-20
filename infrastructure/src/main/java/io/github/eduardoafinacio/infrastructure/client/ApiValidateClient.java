2b522b1858abdf5b4db6567e733789f90aba9706
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-20 16:38:12 -0300

    Creating ApiValidateClient Feign interface to communicate with mocked api.

---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/client/ApiValidateClient.java
@@ -0,0 +1,12 @@
+package io.github.eduardoafinacio.infrastructure.client;
+
+import io.github.eduardoafinacio.infrastructure.client.dto.response.ApiValidateResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "api-validate", url = "${client.url}")
+public interface ApiValidateClient {
+
+    @GetMapping("/ApiValidate")
+    ApiValidateResponse validate();
+}
