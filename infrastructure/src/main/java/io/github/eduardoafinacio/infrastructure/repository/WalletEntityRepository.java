8a1d52c83b67a4ddcd9bd1f9d9346610a6adf4ea
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-20 11:36:17 -0300

    Creating findByUserEntityTaxNumber method in WalletEntityRepository.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/repository/WalletEntityRepository.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/repository/WalletEntityRepository.java
@@ -4,4 +4,5 @@ import io.github.eduardoafinacio.infrastructure.entity.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WalletEntityRepository extends JpaRepository<WalletEntity, Long> {
+    WalletEntity findByUserEntityTaxNumber(String taxNumber);
 }
