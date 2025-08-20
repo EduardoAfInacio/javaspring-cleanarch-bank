792827e88d674f2d6efed6814e09f722234031bd
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-20 20:17:02 -0300

    Creating CreateTransactionGateway implementation.

--- application/src/main/java/io/github/eduardoafinacio/gateway/CreateTransactionGateway.java
+++ application/src/main/java/io/github/eduardoafinacio/gateway/CreateTransactionGateway.java
@@ -1,7 +1,8 @@
 package io.github.eduardoafinacio.gateway;
 
 import io.github.eduardoafinacio.core.domain.Transaction;
+import io.github.eduardoafinacio.core.exception.TaxNumberException;
 
 public interface CreateTransactionGateway {
-    Transaction create(Transaction transaction);
+    Transaction create(Transaction transaction) throws TaxNumberException;
 }
---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/service/CreateTransactionGatewayImpl.java
@@ -0,0 +1,26 @@
+package io.github.eduardoafinacio.infrastructure.service;
+
+import io.github.eduardoafinacio.core.domain.Transaction;
+import io.github.eduardoafinacio.core.exception.TaxNumberException;
+import io.github.eduardoafinacio.gateway.CreateTransactionGateway;
+import io.github.eduardoafinacio.infrastructure.mapper.TransactionMapper;
+import io.github.eduardoafinacio.infrastructure.repository.TransactionEntityRepository;
+
+public class CreateTransactionGatewayImpl implements CreateTransactionGateway {
+    private TransactionEntityRepository transactionEntityRepository;
+    private TransactionMapper transactionMapper;
+
+    public CreateTransactionGatewayImpl(TransactionEntityRepository transactionEntityRepository, TransactionMapper transactionMapper) {
+        this.transactionEntityRepository = transactionEntityRepository;
+        this.transactionMapper = transactionMapper;
+    }
+    @Override
+    public Transaction create(Transaction transaction) {
+        try{
+            var transactionEntitySaved = transactionEntityRepository.save(transactionMapper.toTransactionEntityForCreation(transaction));
+            return transactionMapper.toTransaction(transactionEntitySaved);
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
