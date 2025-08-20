ffa043735e01249901b2f13fc395f8d1fb5bb3a7
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-20 19:46:29 -0300

    Creating TransactionMapper.

---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/mapper/TransactionMapper.java
@@ -0,0 +1,41 @@
+package io.github.eduardoafinacio.infrastructure.mapper;
+
+import io.github.eduardoafinacio.core.domain.Transaction;
+import io.github.eduardoafinacio.core.exception.TaxNumberException;
+import io.github.eduardoafinacio.infrastructure.entity.TransactionEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionMapper {
+    private WalletMapper walletMapper;
+    private UserMapper userMapper;
+    private TransactionPinMapper transactionPinMapper;
+
+    public TransactionMapper(WalletMapper walletMapper, UserMapper userMapper, TransactionPinMapper transactionPinMapper) {
+        this.walletMapper = walletMapper;
+    }
+
+    public TransactionEntity toTransactionEntity(Transaction transaction) {
+        return new TransactionEntity(
+                transaction.getId(),
+                walletMapper.toWalletEntity(transaction.getFromWallet()),
+                walletMapper.toWalletEntity(transaction.getToWallet()),
+                transaction.getValue(),
+                transaction.getStatus(),
+                transaction.getCreatedAt(),
+                transaction.getUpdatedAt()
+        );
+    }
+
+    public Transaction toTransaction(TransactionEntity transactionEntity) throws TaxNumberException {
+        return new Transaction(
+                transactionEntity.getId(),
+                walletMapper.toWallet(transactionEntity.getFromWallet()),
+                walletMapper.toWallet(transactionEntity.getToWallet()),
+                transactionEntity.getTransactionValue(),
+                transactionEntity.getStatus(),
+                transactionEntity.getCreatedAt(),
+                transactionEntity.getUpdatedAt()
+        );
+    }
+}
