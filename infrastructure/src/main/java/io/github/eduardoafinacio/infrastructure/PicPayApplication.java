7af72bd0864fb5f5e5abdd60e3b37540d28816a0
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-20 14:05:49 -0300

    Adding @EnableFeignClient main class.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/PicPayApplication.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/PicPayApplication.java
@@ -2,8 +2,10 @@ package io.github.eduardoafinacio.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PicPayApplication {
     public static void main(String[] args) {
         SpringApplication.run(PicPayApplication.class, args);
