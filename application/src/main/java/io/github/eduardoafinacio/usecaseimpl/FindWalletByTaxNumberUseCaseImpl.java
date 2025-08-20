5fc6a1dfb5b4a3ba0c472d6e936faa6321818a77
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-20 12:58:55 -0300

    Adding TaxNumberException to findByTaxNumber signatures.

--- application/src/main/java/io/github/eduardoafinacio/usecaseimpl/FindWalletByTaxNumberUseCaseImpl.java
+++ application/src/main/java/io/github/eduardoafinacio/usecaseimpl/FindWalletByTaxNumberUseCaseImpl.java
@@ -2,6 +2,7 @@ package io.github.eduardoafinacio.usecaseimpl;
 
 import io.github.eduardoafinacio.core.domain.Wallet;
 import io.github.eduardoafinacio.core.exception.NotFoundException;
+import io.github.eduardoafinacio.core.exception.TaxNumberException;
 import io.github.eduardoafinacio.core.exception.enums.ErrorCodeEnum;
 import io.github.eduardoafinacio.gateway.FindWalletByTaxNumberGateway;
 import io.github.eduardoafinacio.usecase.FindWalletByTaxNumberUseCase;
@@ -13,7 +14,7 @@ public class FindWalletByTaxNumberUseCaseImpl implements FindWalletByTaxNumberUs
         this.findWalletByTaxNumberGateway = findWalletByTaxNumberGateway;
     }
     @Override
-    public Wallet findByTaxNumber(String taxNumber) throws NotFoundException{
+    public Wallet findByTaxNumber(String taxNumber) throws NotFoundException, TaxNumberException {
         var wallet = findWalletByTaxNumberGateway.findByTaxNumber(taxNumber);
         if(wallet == null) {
             throw new NotFoundException(ErrorCodeEnum.WA0001.getMessage(), ErrorCodeEnum.WA0001.getCode());
--- usecase/src/main/java/io/github/eduardoafinacio/usecase/FindWalletByTaxNumberUseCase.java
+++ usecase/src/main/java/io/github/eduardoafinacio/usecase/FindWalletByTaxNumberUseCase.java
@@ -2,7 +2,8 @@ package io.github.eduardoafinacio.usecase;
 
 import io.github.eduardoafinacio.core.domain.Wallet;
 import io.github.eduardoafinacio.core.exception.NotFoundException;
+import io.github.eduardoafinacio.core.exception.TaxNumberException;
 
 public interface FindWalletByTaxNumberUseCase {
-    Wallet findByTaxNumber(String taxNumber) throws NotFoundException;
+    Wallet findByTaxNumber(String taxNumber) throws NotFoundException, TaxNumberException;
 }
