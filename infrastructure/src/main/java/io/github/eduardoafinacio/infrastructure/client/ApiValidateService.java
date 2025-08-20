c25f5bf503e311cf93cbd8d06cd2bbe7b6c1e326
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-20 16:40:05 -0300

    Creating ApiValidateService that calls the ApiValidateService interface.

---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/client/ApiValidateService.java
@@ -0,0 +1,21 @@
+package io.github.eduardoafinacio.infrastructure.client;
+
+import io.github.eduardoafinacio.infrastructure.client.dto.response.ApiValidateResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiValidateService {
+    private ApiValidateClient apiValidateClient;
+
+    public ApiValidateService(ApiValidateClient apiValidateClient) {
+        this.apiValidateClient = apiValidateClient;
+    }
+
+    public ApiValidateResponse validate(){
+        try{
+            return apiValidateClient.validate();
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
