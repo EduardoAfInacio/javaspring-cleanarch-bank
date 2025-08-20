a86792bdf153555067c0ebf4616b1987c008f371
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-20 11:34:10 -0300

    Updating WalletMapper.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/mapper/WalletMapper.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/mapper/WalletMapper.java
@@ -1,6 +1,7 @@
 package io.github.eduardoafinacio.infrastructure.mapper;
 
 import io.github.eduardoafinacio.core.domain.Wallet;
+import io.github.eduardoafinacio.core.exception.TaxNumberException;
 import io.github.eduardoafinacio.infrastructure.entity.TransactionPinEntity;
 import io.github.eduardoafinacio.infrastructure.entity.UserEntity;
 import io.github.eduardoafinacio.infrastructure.entity.WalletEntity;
@@ -8,6 +9,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class WalletMapper {
+    private UserMapper userMapper;
+    private TransactionPinMapper transactionPinMapper;
+
+    public WalletMapper(UserMapper userMapper, TransactionPinMapper transactionPinMapper) {
+        this.userMapper = userMapper;
+        this.transactionPinMapper = transactionPinMapper;
+    }
+
     public WalletEntity toWalletEntity(Wallet wallet, UserEntity userEntity, TransactionPinEntity transactionPinEntity){
         return new WalletEntity(
                 wallet.getBalance(),
@@ -15,6 +24,18 @@ public class WalletMapper {
                 transactionPinEntity,
                 wallet.getCreatedAt(),
                 wallet.getUpdatedAt()
+        );
+    }
+
+    public Wallet toWallet(WalletEntity walletEntity) throws TaxNumberException {
+        if(walletEntity == null) return null;
+
+        return new Wallet(
+                walletEntity.getId(),
+                transactionPinMapper.toTransactionPin(walletEntity.getTransactionPinEntity()),
+                walletEntity.getBalance(),
+                userMapper.toUser(walletEntity.getUserEntity()),
+
         );
     }
 }
