e67ef89f44313fb87b490122ee1f10a301a1c0a2
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-22 22:16:42 -0300

    Creating Wallet Controller.

---
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/controller/WalletController.java
@@ -0,0 +1,26 @@
+package io.github.eduardoafinacio.infrastructure.controller;
+
+import io.github.eduardoafinacio.core.exception.NotFoundException;
+import io.github.eduardoafinacio.core.exception.TaxNumberException;
+import io.github.eduardoafinacio.infrastructure.dto.response.BaseResponse;
+import io.github.eduardoafinacio.infrastructure.dto.response.ConsultBalanceResponse;
+import io.github.eduardoafinacio.usecase.ConsultBalanceUseCase;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/wallet")
+public class WalletController {
+    private ConsultBalanceUseCase consultBalanceUseCase;
+
+    public WalletController(ConsultBalanceUseCase consultBalanceUseCase) {
+        this.consultBalanceUseCase = consultBalanceUseCase;
+    }
+    @GetMapping("/consultBalance/{taxNumber}")
+    public BaseResponse<ConsultBalanceResponse> consultBalance(@PathVariable String taxNumber) throws TaxNumberException, NotFoundException {
+        var balance = consultBalanceUseCase.consult(taxNumber);
+        return BaseResponse.<ConsultBalanceResponse>builder().result(new ConsultBalanceResponse(balance)).build();
+    }
+}
