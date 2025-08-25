51bd83646464c06b52ac824b7afb61ce063731fd
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-25 15:20:43 -0300

    Creating Beans to various use cases.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/config/WalletConfig.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/config/WalletConfig.java
@@ -1,10 +1,8 @@
 package io.github.eduardoafinacio.infrastructure.config;
 
-import io.github.eduardoafinacio.gateway.FindWalletByTaxNumberGateway;
-import io.github.eduardoafinacio.usecase.ConsultBalanceUseCase;
-import io.github.eduardoafinacio.usecase.FindWalletByTaxNumberUseCase;
-import io.github.eduardoafinacio.usecaseimpl.ConsultBalanceUseCaseImpl;
-import io.github.eduardoafinacio.usecaseimpl.FindWalletByTaxNumberUseCaseImpl;
+import io.github.eduardoafinacio.gateway.*;
+import io.github.eduardoafinacio.usecase.*;
+import io.github.eduardoafinacio.usecaseimpl.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,5 +16,35 @@ public class WalletConfig {
     @Bean
     public ConsultBalanceUseCase consultBalanceUseCase(FindWalletByTaxNumberUseCase findWalletByTaxNumberUseCase){
         return new ConsultBalanceUseCaseImpl(findWalletByTaxNumberUseCase);
+    }
+
+    @Bean
+    public UpdateTransactionPinUseCase updateTransactionPinUseCase(UpdateTransactionPinGateway updateTransactionPinGateway){
+        return new UpdateTransactionPinUseCaseImpl(updateTransactionPinGateway);
+    }
+
+    @Bean
+    public TransactionPinValidateUseCase transactionPinValidateUseCase(TransactionPinValidateGateway transactionPinValidateGateway, UpdateTransactionPinUseCase updateTransactionPinUseCase){
+        return new TransactionPinValidateUseCaseImpl(transactionPinValidateGateway, updateTransactionPinUseCase);
+    }
+
+    @Bean
+    public UserNotificationUseCase userNotificationUseCase(UserNotificationGateway userNotificationGateway){
+        return new UserNotificationUseCaseImpl(userNotificationGateway);
+    }
+
+    @Bean
+    public TransactionValidateUseCase transactionValidateUseCase(TransactionValidateGateway transactionValidateGateway){
+        return new TransactionValidateUseCaseImpl(transactionValidateGateway);
+    }
+
+    @Bean
+    public CreateTransactionUseCase createTransactionUseCase(CreateTransactionGateway createTransactionGateway){
+        return new CreateTransactionUseCaseImpl(createTransactionGateway);
+    }
+
+    @Bean
+    public TransferUseCase transferUseCase(FindWalletByTaxNumberUseCase findWalletByTaxNumberUseCase, CreateTransactionUseCase createTransactionUseCase, TransactionValidateUseCase transactionValidateUseCase, TransferGateway transferGateway, UserNotificationUseCase userNotificationUseCase, TransactionPinValidateUseCase transactionPinValidateUseCase){
+        return new TransferUseCaseImpl(findWalletByTaxNumberUseCase, createTransactionUseCase, transactionValidateUseCase, transferGateway, userNotificationUseCase, transactionPinValidateUseCase);
     }
 }
