49fd3c4156a3b06464bbb338ff80bc37b3f69c98
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-25 18:53:18 -0300

    Removing useless imports.

--- usecase/src/main/java/io/github/eduardoafinacio/usecase/TransferUseCase.java
+++ usecase/src/main/java/io/github/eduardoafinacio/usecase/TransferUseCase.java
@@ -1,10 +1,7 @@
 package io.github.eduardoafinacio.usecase;
 
 import io.github.eduardoafinacio.core.domain.Transaction;
-import io.github.eduardoafinacio.core.domain.User;
 import io.github.eduardoafinacio.core.exception.*;
-
-import java.math.BigDecimal;
 
 public interface TransferUseCase {
     Boolean transfer(Transaction transaction) throws TransferException, NotFoundException, NotificationException, InternalServerErrorException, TransactionPinException, TaxNumberException;
