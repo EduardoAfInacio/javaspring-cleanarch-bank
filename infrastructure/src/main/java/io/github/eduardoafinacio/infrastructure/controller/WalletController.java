abd67ca3dbb86cf448fb2d28b1cdaccc1d4aee47
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-25 18:52:19 -0300

    Refactoring WalletController.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/controller/WalletController.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/controller/WalletController.java
@@ -1,11 +1,11 @@
 package io.github.eduardoafinacio.infrastructure.controller;
 
+import io.github.eduardoafinacio.core.domain.TransactionPin;
 import io.github.eduardoafinacio.core.exception.*;
 import io.github.eduardoafinacio.infrastructure.dto.request.TransferRequest;
 import io.github.eduardoafinacio.infrastructure.dto.response.BaseResponse;
 import io.github.eduardoafinacio.infrastructure.dto.response.ConsultBalanceResponse;
-import io.github.eduardoafinacio.usecase.ConsultBalanceUseCase;
-import io.github.eduardoafinacio.usecase.TransferUseCase;
+import io.github.eduardoafinacio.usecase.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,10 +13,18 @@ import org.springframework.web.bind.annotation.*;
 public class WalletController {
     private ConsultBalanceUseCase consultBalanceUseCase;
     private TransferUseCase transferUseCase;
+    private CreateTransactionUseCase createTransactionUseCase;
+    private FindWalletByTaxNumberUseCase findWalletByTaxNumberUseCase;
+    private TransactionValidateUseCase transactionValidateUseCase;
+    private TransactionPinValidateUseCase transactionPinValidateUseCase;
 
-    public WalletController(ConsultBalanceUseCase consultBalanceUseCase, TransferUseCase transferUseCase) {
+    public WalletController(ConsultBalanceUseCase consultBalanceUseCase, TransferUseCase transferUseCase, CreateTransactionUseCase createTransactionUseCase, FindWalletByTaxNumberUseCase findWalletByTaxNumberUseCase, TransactionValidateUseCase transactionValidateUseCase, TransactionPinValidateUseCase transactionPinValidateUseCase) {
         this.consultBalanceUseCase = consultBalanceUseCase;
         this.transferUseCase = transferUseCase;
+        this.createTransactionUseCase = createTransactionUseCase;
+        this.findWalletByTaxNumberUseCase = findWalletByTaxNumberUseCase;
+        this.transactionValidateUseCase = transactionValidateUseCase;
+        this.transactionPinValidateUseCase = transactionPinValidateUseCase;
     }
 
     @GetMapping("/consultBalance/{taxNumber}")
@@ -27,7 +35,18 @@ public class WalletController {
 
     @PostMapping("/transfer")
     public BaseResponse<String> transfer(@RequestBody TransferRequest request) throws TransferException, TaxNumberException, NotificationException, NotFoundException, InternalServerErrorException, TransactionPinException {
-        transferUseCase.transfer(request.fromTaxNumber(), request.toTaxNumber(), request.value(), request.pin());
+
+        var from = findWalletByTaxNumberUseCase.findByTaxNumber(request.fromTaxNumber());
+
+        transactionPinValidateUseCase.validate(from.getTransactionPin(), request.pin());
+
+        var to = findWalletByTaxNumberUseCase.findByTaxNumber(request.toTaxNumber());
+
+        var transaction = createTransactionUseCase.create(from, to, request.value());
+
+        transactionValidateUseCase.validate(transaction);
+
+        transferUseCase.transfer(transaction);
         return BaseResponse.<String>builder().success(true).message("Transferred successfully").build();
     }
 }
