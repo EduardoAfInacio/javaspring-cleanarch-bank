f959c13a3a68275407ac3f03a70fb5241d8a8add
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-25 18:39:15 -0300

    Refactoring CreateTransactionUseCase.

--- application/src/main/java/io/github/eduardoafinacio/usecaseimpl/CreateTransactionUseCaseImpl.java
+++ application/src/main/java/io/github/eduardoafinacio/usecaseimpl/CreateTransactionUseCaseImpl.java
@@ -1,11 +1,14 @@
 package io.github.eduardoafinacio.usecaseimpl;
 
 import io.github.eduardoafinacio.core.domain.Transaction;
+import io.github.eduardoafinacio.core.domain.Wallet;
 import io.github.eduardoafinacio.core.exception.TaxNumberException;
 import io.github.eduardoafinacio.core.exception.TransferException;
 import io.github.eduardoafinacio.core.exception.enums.ErrorCodeEnum;
 import io.github.eduardoafinacio.gateway.CreateTransactionGateway;
 import io.github.eduardoafinacio.usecase.CreateTransactionUseCase;
+
+import java.math.BigDecimal;
 
 public class CreateTransactionUseCaseImpl implements CreateTransactionUseCase {
     private CreateTransactionGateway createTransactionGateway;
@@ -14,7 +17,8 @@ public class CreateTransactionUseCaseImpl implements CreateTransactionUseCase {
         this.createTransactionGateway = createTransactionGateway;
     }
     @Override
-    public Transaction create(Transaction transaction) throws TransferException, TaxNumberException {
+    public Transaction create(Wallet from, Wallet to, BigDecimal value) throws TransferException, TaxNumberException {
+        var transaction = new Transaction(from, to, value);
         var transactionSaved = createTransactionGateway.create(transaction);
         if(transactionSaved == null) {
             throw new TransferException(ErrorCodeEnum.TR0003.getMessage(), ErrorCodeEnum.TR0003.getCode());
--- usecase/src/main/java/io/github/eduardoafinacio/usecase/CreateTransactionUseCase.java
+++ usecase/src/main/java/io/github/eduardoafinacio/usecase/CreateTransactionUseCase.java
@@ -1,9 +1,12 @@
 package io.github.eduardoafinacio.usecase;
 
 import io.github.eduardoafinacio.core.domain.Transaction;
+import io.github.eduardoafinacio.core.domain.Wallet;
 import io.github.eduardoafinacio.core.exception.TaxNumberException;
 import io.github.eduardoafinacio.core.exception.TransferException;
 
+import java.math.BigDecimal;
+
 public interface CreateTransactionUseCase {
-    Transaction create(Transaction transaction) throws TransferException, TaxNumberException;
+    Transaction create(Wallet from, Wallet to, BigDecimal value) throws TransferException, TaxNumberException;
 }
