75dc1a07b2b933527572ae2acdb6a67a08a13478
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-25 22:47:51 -0300

    Fixing FeignClient parameters.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/PicPayApplication.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/PicPayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "io.github.eduardoafinacio.infrastructure.client")
 public class PicPayApplication {
     public static void main(String[] args) {
         SpringApplication.run(PicPayApplication.class, args);
