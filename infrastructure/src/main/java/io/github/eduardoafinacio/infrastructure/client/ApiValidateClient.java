f08117c2cbcbade228135aeb79ce3f8528ede5f5
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-25 22:23:02 -0300

    Fixing FeignClient parameters.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/client/ApiValidateClient.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/client/ApiValidateClient.java
@@ -4,9 +4,9 @@ import io.github.eduardoafinacio.infrastructure.client.dto.response.ApiValidateR
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "api-validate", url = "${client.url}")
+@FeignClient(name = "/ApiValidate", url = "${client.url}")
 public interface ApiValidateClient {
 
-    @GetMapping("/ApiValidate")
+    @GetMapping
     ApiValidateResponse validate();
 }
