2e20decd357147a86d803b98bd4ba658e4f3d86a
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-28 15:59:02 -0300

    Adding Validation annotations to CreateUserRequest record.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/dto/request/CreateUserRequest.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/dto/request/CreateUserRequest.java
@@ -1,6 +1,8 @@
 package io.github.eduardoafinacio.infrastructure.dto.request;
 
 import io.github.eduardoafinacio.core.domain.enums.UserTypeEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CreateUserRequest(String email, String password, String taxNumber, String fullname, UserTypeEnum type, String pin) {
+public record CreateUserRequest(@NotBlank String email, @NotBlank String password, @NotBlank String taxNumber, @NotBlank String fullname, @NotNull UserTypeEnum type, @NotBlank String pin) {
 }
