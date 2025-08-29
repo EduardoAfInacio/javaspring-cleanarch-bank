0f2ec054eafdcf813db034b8f92fdfec2c16a647
EduardoAfInacio <eduafinacio@gmail.com>
2025-08-29 14:20:26 -0300

    Changing App name.

--- infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/BankApplication.java
+++ infrastructure/src/main/java/io/github/eduardoafinacio/infrastructure/BankApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "io.github.eduardoafinacio.infrastructure.client")
-public class PicPayApplication {
+public class BankApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PicPayApplication.class, args);
+        SpringApplication.run(BankApplication.class, args);
     }
 }
